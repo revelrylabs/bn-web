54bbb1ee96a84c1086db9eb60a862398abf28f33
Brad Huber <daybreaker@gmail.com>
2018-11-16 11:29:14 -0600

    Converting prefix to an env variable

--- src/helpers/bigneon.js
+++ src/helpers/bigneon.js
@@ -11,7 +11,7 @@ export const bigneonFactory = (options = {}, headers = {}, mockData) => {
 				port: process.env.REACT_APP_API_PORT,
 				timeout: process.env.REACT_APP_API_TIMEOUT || 30000,
 				basePath: process.env.REACT_APP_API_BASEPATH || "",
-				prefix: "https://thingproxy.freeboard.io/fetch/https://staging.bigneon.com/api/"
+				prefix: process.env.REACT_APP_API_PREFIX || "",
 			},
 			...options
 		};
